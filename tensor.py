06172691405de2e7dbedcaf83458f36614152b8e
mohamed-180 <mohamed_d180@hotmail.com>
2023-09-10 14:46:50 +0300

    logistic regression

--- tensor.py
+++ tensor.py
@@ -2,8 +2,7 @@ import torch
 import numpy as np
 from time import time
 
-
-
+#Linear regression
 x = torch.linspace(-3, 3, 1000)
 y = 3.45*x + 1.3 + torch.randn(1000)
 
@@ -21,3 +20,17 @@ for i in range(1000):
 	b.grad.zero_()
 print(time() - t)
 
+
+#Logistic regression
+
+w = torch.randn(1, requires_grad=True)
+b = torch.tensor(0.0, requires_grad=True)
+for i in range(7000):
+    yhat = 1./(1.+ torch.exp(-w * x - b))
+    l = loss(y, yhat)
+    l.backward()
+    with torch.no_grad():
+        w -= .01*w.grad
+        b -= .01*b.grad 
+    w.grad.zero_()
+    b.grad.zero_()
